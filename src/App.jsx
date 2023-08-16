7545f04529a16e238cb2d7386eadd4b36085ed06
Darpan3011 <darpankanani301@gmail.com>
2023-08-16 11:41:24 +0530

    warning

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,39 @@ import git from "./github.svg";
 import linked from "./linkedin.svg";
 import leetcode from "./leetcode.svg";
 import { useState } from "react";
+import close from './close.svg';
+
 
 function App() {
   const [button, setButton] = useState("ast");
-
+  
   function asd() {
     window.location.href = "https://leetcode.com/Darpan301/";
   }
-
+  
   function asd1() {
     window.location.href = "https://github.com/Darpan3011";
   }
-
+  
   function asd2() {
     window.location.href = "https://linkedin.com/in/darpan-kanani";
   }
-
+  
   function handleClick(e) {
     setButton(e);
   }
-
+  
+  const [b1, setb1] = useState(true);
+  function warning() {
+    setb1(false);
+  }
+  
   return (
     <>
+      {b1&&<div className="warning">
+        <p>Project Under Development.</p>
+        <img className="dj" src={close} alt="close" height={15} onClick={warning}></img>
+      </div>}
       <div className="container">
         <div className="sidemenu">
           <div className="imagelogo">
@@ -64,21 +75,9 @@ function App() {
               <button className="cm">Contact Me</button>
             </div>
           )}
-          {button === "gfg" && (
-            <div className="content">
-              No skills
-            </div>
-          )}
-          {button === "ddf" && (
-            <div className="content">
-              Dasdasd
-            </div>
-          )}
-          {button === "xyz" && (
-            <div className="content">
-              asdasd
-            </div>
-          )}
+          {button === "gfg" && <div className="content">No skills</div>}
+          {button === "ddf" && <div className="content">Dasdasd</div>}
+          {button === "xyz" && <div className="content">asdasd</div>}
         </div>
       </div>
     </>
